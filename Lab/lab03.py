c37fa4e0ee8701427b8d5b8f737e6db8bc1338af
Ita_Hloks <115126342+Ita-Hloks@users.noreply.github.com>
2024-09-26 20:25:52 +0800

    Add files via upload

---
+++ Lab/lab03.py
@@ -0,0 +1,179 @@
+LAB_SOURCE_FILE = __file__
+
+
+def double_eights(n):
+    """ Returns whether or not n has two digits in row that
+    are the number 8. Assume n has at least two digits in it.
+
+    >>> double_eights(1288)
+    True
+    >>> double_eights(880)
+    True
+    >>> double_eights(538835)
+    True
+    >>> double_eights(284682)
+    False
+    >>> double_eights(588138)
+    True
+    >>> double_eights(78)
+    False
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(LAB_SOURCE_FILE, 'double_eights', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    n, remainder = n // 10, n % 10
+    if n % 10 == remainder == 8:
+        return True
+    elif (n > 0):
+        return double_eights(n)
+    return False
+
+
+def make_onion(f, g):
+    """Return a function can_reach(x, y, limit) that returns
+    whether some call expression containing only f, g, and x with
+    up to limit calls will give the result y.
+
+    >>> up = lambda x: x + 1
+    >>> double = lambda y: y * 2
+    >>> can_reach = make_onion(up, double)
+    >>> can_reach(5, 25, 4)      # 25 = up(double(double(up(5))))
+    True
+    >>> can_reach(5, 25, 3)      # Not possible
+    False
+    >>> can_reach(1, 1, 0)      # 1 = 1
+    True
+    >>> add_ing = lambda x: x + "ing"
+    >>> add_end = lambda y: y + "end"
+    >>> can_reach_string = make_onion(add_ing, add_end)
+    >>> can_reach_string("cry", "crying", 1)      # "crying" = add_ing("cry")
+    True
+    >>> can_reach_string("un", "unending", 3)     # "unending" = add_ing(add_end("un"))
+    True
+    >>> can_reach_string("peach", "folding", 4)   # Not possible
+    False
+    """
+
+    def can_reach(x, y, limit):
+        if limit < 0:
+            return False
+        elif x == y:
+            return True
+        else:
+            return can_reach(f(x), y, limit - 1) or can_reach(g(x), y, limit - 1)
+
+    return can_reach
+
+
+def mario_number(level):
+    """Return the number of ways that Mario can perform a sequence of steps
+    or jumps to reach the end of the level without ever landing in a Piranha
+    plant. Assume that every level begins and ends with a space.
+
+    >>> mario_number(' P P ')   # jump, jump
+    1
+    >>> mario_number(' P P  ')   # jump, jump, step
+    1
+    >>> mario_number('  P P ')  # step, jump, jump
+    1
+    >>> mario_number('   P P ') # step, step, jump, jump or jump, jump, jump
+    2
+    >>> mario_number(' P PP ')  # Mario cannot jump two plants
+    0
+    >>> mario_number('    ')    # step, jump ; jump, step ; step, step, step
+    3
+    >>> mario_number('    P    ')
+    9
+    >>> mario_number('   P    P P   P  P P    P     P ')
+    180
+    """
+    "*** YOUR CODE HERE ***"
+    if level == '' or level[0] == 'P':
+        return 0
+    elif level == ' ':
+        return 1
+
+    def step(level):
+        return level[1:]
+
+    def jump(level):
+        return level[2:]
+
+    return mario_number(step(level)) + mario_number(jump(level))
+
+
+def max_subseq(n, t):
+    """
+
+    and of these, the maxumum number is 22, so our answer is 22.
+
+    >>> max_subseq(2012, 2)
+    22
+    >>> max_subseq(20125, 3)
+    225
+    >>> max_subseq(20125, 5)
+    20125
+    >>> max_subseq(20125, 6) # note that 20125 == 020125
+    20125
+    >>> max_subseq(12345, 3)
+    345
+    >>> max_subseq(12345, 0) # 0 is of length 0
+    0
+    >>> max_subseq(12345, 1)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if t == 0:
+        return 0
+    # 组装为列表
+    list = [int(x) for x in str(n)]
+    remove = len(list) - t
+    while remove > 0:
+        list = [x for x in list if x != min(list)]
+        remove -= 1
+    new_num = 0
+    while len(list) >= 1:
+        new_num = new_num * 10 + list[0]
+        list = [x for x in list[1:]]
+    return new_num
+    # ===========================
+    # 初代版本，没必要看
+    # if t == 0:
+    #     return 0
+    # # 组装为列表
+    # list = [int(x) for x in str(n)]
+    # remove = len(list) - t
+    # while remove > 0:
+    #     list = [x for x in list if x != min(list)]
+    #     remove -= 1
+    # list.reverse()
+    # new_num = 0
+    # i = 1
+    # while len(list) >= 1:
+    #     new_num += list[0] * i
+    #     i *= 10
+    #     list = [x for x in list[1:]]
+    # return new_num
+
+
+def is_prime(n):
+    """
+    >>> is_prime(7)
+    True
+    >>> is_prime(10)
+    False
+    >>> is_prime(1)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    def prime_helper(index):
+        if index == n:
+            return True
+        elif n % index == 0 or n == 1:
+            return False
+        else:
+            return prime_helper(index + 1)
+    return prime_helper(2)
+
